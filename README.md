2341581b2cb220fbd784449a708cececc8aa6eb1
Caio Marcellos <caiocuritiba@gmail.com>
2017-08-03 20:52:04 -0300

    allset

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # hdf52json_cm
 
-Convert hdf5 to json functions
+Convert hdf5 to json functions (auxiliary package for personal use)
+
+## Installation
+
+Get from github
+
+	git clone https://github.com/caiofcm/hdf52json_cm.git
+
+Install with pip (as developer mode (if not remove -e))
+	pip install -e .
+
+	python
+	import hdf52json_cm
+	hdf52json_cm.testing()
--- hdf52json_cm/hdf52json_cm.py
+++ hdf52json_cm/hdf52json_cm.py
@@ -1,9 +1,15 @@
 import numpy as np
 import h5py
+import json
 
 #################################################################
 ################# FROM DICTIONARY TO H5PY #############
 #################################################################
+def hdf52json(hdfileName, jsonFName):
+	dicH5 = load_dict_from_hdf5_listing(hdfileName)
+	with open(jsonFName, 'w') as fp:
+		json.dump(dicH5, fp)
+	pass
 
 def save_dict_to_hdf5(dic, filename):
 	with h5py.File(filename, 'w') as h5file:
@@ -15,6 +21,10 @@ def load_dict_from_hdf5(filename):
 		return recursively_load_dict_contents_from_group(h5file, '/')
 	pass
 
+def load_dict_from_hdf5_listing(filename):
+	with h5py.File(filename, 'r') as h5file:
+		return recursively_load_dict_contents_from_group_listing(h5file, '/')
+	pass
 
 def recursively_save_dict_contents_to_group( h5file, path, dic):
 
@@ -56,8 +66,8 @@ def recursively_save_dict_contents_to_group( h5file, path, dic):
 				except:
 					item = np.array(item).astype('|S9')
 					h5file[path + key] = item
-				if not np.array_equal(h5file[path + key].value, item):
-					raise ValueError('The data representation in the HDF5 file does not match the original dict.')
+				# if not np.array_equal(h5file[path + key].value, item):
+				# 	raise ValueError('The data representation in the HDF5 file does not match the original dict.')
 		# save dictionaries
 		elif isinstance(item, dict):
 			recursively_save_dict_contents_to_group(h5file, path + key + '/', item)
@@ -76,14 +86,21 @@ def recursively_load_dict_contents_from_group( h5file, path):
 			ans[key] = recursively_load_dict_contents_from_group(h5file, path + key + '/')
 	return ans  
 
+def recursively_load_dict_contents_from_group_listing( h5file, path): 
 
-#####################################
-#####################################
-##############TESTS################
-#####################################
-if __name__ == '__main__':
+	ans = {}
+	for key, item in h5file[path].items():
+		if isinstance(item, h5py._hl.dataset.Dataset):
+			if isinstance(item.value, np.ndarray):
+				ans[key] = item.value.tolist()
+			else:
+				ans[key] = item.value
+		elif isinstance(item, h5py._hl.group.Group):
+			ans[key] = recursively_load_dict_contents_from_group_listing(h5file, path + key + '/')
+	return ans 
 
 
+def testing():
 #-----------------------------------------
 	# HDF5 SAVE FROM DICT TEST
 	fileName = 'test_nested.h5'
@@ -91,8 +108,23 @@ if __name__ == '__main__':
 		'co' : np.linspace(10, 1.0),
 		'co2' : np.linspace(20, 50.0),
 		'in' : {'f':[1,2,3,4], 'q': 'sabia'}}
-	#save_dict_to_hdf5(d, fileName)
+	save_dict_to_hdf5(d, fileName)
 #-----------------------------------------	
+
+#-----------------------------------------
+	# HDF5 LOAD SAVE TO JSON
+	fileName = 'h5datasetMatrix.h5'
+	jsnFile = 'testejson.json'
+	hdf52json(fileName, jsnFile)
+#-----------------------------------------
+
+#####################################
+#####################################
+##############TESTS################
+#####################################
+if __name__ == '__main__':
+	testing()
+
 
 
 	pass
